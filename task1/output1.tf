a82c083c3b1ca4832e2d19ffd36a4929d3e6d764
dasanirban834 <67305577+dasanirban834@users.noreply.github.com>
2021-04-20 10:09:17 +0530

    Add files via upload

---
+++ task1/output1.tf
@@ -0,0 +1,31 @@
+output "enviroment" {
+    value = local.environment
+}
+
+output "os" {
+  value = local.os
+}
+
+output "region" {
+  value = local.region
+}
+
+output "zone1a" {
+  value = local.zone1
+}
+
+output "zone2a" {
+  value = local.zone2
+}
+
+output "zone3a" {
+  value = local.zone3
+}
+
+output "zonea" {
+  value = local.zone
+}
+
+output "syntax" {
+  value = local.syntax
+}
---
+++ task1/variable1.tf
@@ -0,0 +1,16 @@
+variable "environment" {
+        default =  "production"
+           
+}
+
+variable "os" {
+        default = "windows" 
+}
+
+variable "region" {
+        default = "europe-"  
+}
+
+variable "zone" {
+        default = "west4-a" 
+}
