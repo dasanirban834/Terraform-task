9053b73733beba9793092bdbbcd368136d69ce7f
dasanirban834 <67305577+dasanirban834@users.noreply.github.com>
2021-04-28 10:28:15 +0530

    Add files via upload

--- task2/main2.tf
+++ task2/main2.tf
@@ -1,12 +1,15 @@
+######  Write a script to create naming convention in that format <region> <aAZ> <environemnt> <os> <serial-no> <drive-mount> <disk>    #######
+
+
 locals {
-  environment = substr (var.environment, 0, 1)
-  os          = substr (var.os, 0, 1)
-  region      = substr (var.region, 0, 2)
-  zone1       = substr (element(split ("-", var.zone), 1), 0, 1)
-  zone2       = element(split ("", (element(split ("-", var.zone), 1))), 4)
-  zone3       = element(split("-", var.zone), 2)
-  zone        = "${local.zone1}${local.zone2}${local.zone3}"
-  serial-no   = var.serial-no
+  environment = substr (var.environment, 0, 1)                                      # output will be "p"
+  os          = substr (var.os, 0, 1)                                               # output will be "w"
+  region      = substr (var.region, 0, 2)                                           # output will be "us"
+  zone1       = substr (element(split ("-", var.zone), 1), 0, 1)                    # output will be "w"
+  zone2       = element(split ("", (element(split ("-", var.zone), 1))), 4)         # output will be "1"
+  zone3       = element(split("-", var.zone), 2)                                    # output will be "a"
+  zone        = "${local.zone1}${local.zone2}${local.zone3}"                        # output will be "w1a"
+  serial-no   = var.serial-no                                                       
   drive-mount = var.drive-mount
   disk        = var.disk
 
