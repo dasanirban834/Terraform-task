59352d6b5f6d6f3eae954cd95a7b34c575d34719
dasanirban834 <67305577+dasanirban834@users.noreply.github.com>
2021-11-01 12:31:49 +0530

    Update userdata.sh

--- AWS-infrastructure-with-alb/userdata.sh
+++ AWS-infrastructure-with-alb/userdata.sh
@@ -4,6 +4,6 @@ yum update -y
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo 'Welcome to Instance-${count.index}' >> /var/www/html/index.html
+echo 'Welcome to Instance' >> /var/www/html/index.html
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl restart sshd
