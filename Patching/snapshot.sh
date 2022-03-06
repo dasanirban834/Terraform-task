8f7f4c49dfa5f8ffffb32d096b4792ccc2828cbe
dasanirban834 <67305577+dasanirban834@users.noreply.github.com>
2022-03-06 21:10:57 +0530

    Update snapshot.sh

--- Patching/snapshot.sh
+++ Patching/snapshot.sh
@@ -1,1 +1,9 @@
-
+#/bin/bash
+volume=('vol-0b35b9ed5569f4eae' 'vol-03efc7ba00eb1488e' 'vol-0ac738d0953101894')
+i=0
+while [ $i -lt ${#volume[@]} ]
+do
+        echo "Taking Pre-Patching Snapshot of VolumeID: ${volume[$i]}"
+        aws ec2 create-snapshot --volume-id ${volume[$i]}
+        ((i++))
+done
